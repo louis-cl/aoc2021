2f1499fbba91f3f03278391f12bac81ef8de1dc7
Louis Clergue <louisclergue@gmail.com>
2021-12-03 23:15:03 +0100

    Better day3 part2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 use itertools::Itertools;
 
 const SAMPLE: &str = r#"
@@ -20,115 +20,50 @@ fn read_test(input: &str) -> impl Iterator<Item=&str> {
     input.lines().filter(|line| !line.is_empty())
 }
 
-fn o2(lines: Vec<Vec<char>>, i: usize) -> String {
-    if lines.len() == 1 {
-        let r: String = lines[0].iter().collect();
-        return r[i..].to_string();
-    }
-    if lines[0].len() <= i {
-        return String::from("");
-    }
-
-    let n = lines.len();
-    let ones = lines.iter().filter(|c| c[i] == '1').count();
-    let zeros = n - ones;
-
-    if ones >= zeros {
-        // keep 1
-        let rest = o2(
-            lines.into_iter().filter(| c | c[i] == '1').collect(),
-            i+1);
-        String::from("1") + rest.as_str()
-    } else {
-        // keep 0
-        let rest = o2(
-            lines.into_iter().filter(| c | c[i] == '0').collect(),
-            i+1);
-        String::from("0") + rest.as_str()
-    }
+fn zeros_and_ones(chars: &Vec<Vec<char>>, i: usize) -> (usize, usize) {
+    let one_count = chars.iter().map(|bin| bin[i])
+        .filter(|c| *c == '1')
+        .count();
+    (chars.len() - one_count, one_count)
 }
 
-fn co2(lines: Vec<Vec<char>>, i: usize) -> String {
-    if lines.len() == 1 {
-        let r: String = lines[0].iter().collect();
-        return r[i..].to_string();
+fn reducing(lines: Vec<Vec<char>>, rule: impl Fn(usize, usize) -> char) -> String {
+    let mut options: HashSet<_> = lines.into_iter().collect();
+    let mut i = 0;
+    while options.len() > 1 {
+        let groups = options.into_iter().into_group_map_by(|line| line[i]);
+        let (z,o) = (groups[&'0'].len(), groups[&'1'].len());
+        options = groups[&rule(z, o)].clone().into_iter().collect();
+        i += 1;
     }
-
-    if lines[0].len() <= i {
-        return String::from("");
-    }
-
-    let n = lines.len();
-    let ones = lines.iter().filter(|c| c[i] == '1').count();
-    let zeros = n - ones;
-
-    if ones < zeros {
-        // keep 1
-        let rest = co2(
-            lines.into_iter().filter(| c | c[i] == '1').collect(),
-            i+1);
-        String::from("1") + rest.as_str()
-    } else {
-        // keep 0
-        let rest = co2(
-            lines.into_iter().filter(| c | c[i] == '0').collect(),
-            i+1);
-        String::from("0") + rest.as_str()
-    }
+    options.into_iter().next().unwrap().into_iter().collect()
 }
 
 fn main() {
     // let lines: Vec<_> = include_str!("../../input/day3").lines().collect();
-    let lines: Vec<_> =
+    let lines: Vec<Vec<char>> =
         // read_test(SAMPLE)
         include_str!("../../input/day3").lines()
-        .map(|x| x.to_string())
+        .map(|x| x.chars().collect())
         .collect();
 
     let p1 = {
-        let mut zeroCount = HashMap::new();
-        let mut oneCount = HashMap::new();
+        let gamma: String = (0..lines[0].len()).map(|i| {
+            let (z, o) = zeros_and_ones(&lines, i);
+            if o >= z { '1' } else { '0' }
+        }).collect();
 
-        let n = lines[0].len();
+        let g = u32::from_str_radix(gamma.as_str(), 2).unwrap();
+        let e = !g & ((1 << gamma.len()) - 1);
 
-        for l in &lines {
-            for (i, c) in l.chars().enumerate() {
-                if c == '0' {
-                    *zeroCount.entry(i).or_insert(0) += 1;
-                } else {
-                    *oneCount.entry(i).or_insert(0) += 1;
-                }
-            }
-        }
-
-        let mut gamma = String::new();
-        let mut eps = String::new();
-
-        for i in 0..n {
-            let z = zeroCount.get(&i).unwrap_or(&0);
-            let o = oneCount.get(&i).unwrap_or(&0);
-            if z > o {
-                gamma.push('0');
-                eps.push('1');
-            } else {
-                gamma.push('1');
-                eps.push('0');
-            }
-        }
-
-        let g = i64::from_str_radix(gamma.as_str(), 2).unwrap();
-        let e = i64::from_str_radix(eps.as_str(), 2).unwrap();
-
-        println!("{:?} {:?}", g, e);
+        println!("gamma = {:?}, epsilon = {:?}", g, e);
         g * e
     };
-
     println!("p1 = {:?}", p1);
 
     let p2 = {
-        let chars: Vec<Vec<char>> = lines.iter().map(|x| x.chars().collect()).collect();
-        let o2= o2(chars.clone(), 0);
-        let co2= co2(chars, 0);
+        let o2= reducing(lines.clone(), |z, o| if o >= z { '1' } else { '0' });
+        let co2 = reducing(lines, |z, o| if z <= o { '0' } else { '1' });
         println!("o2 = {}, co2 = {}", o2, co2);
         i32::from_str_radix(o2.as_str(), 2).unwrap() *
             i32::from_str_radix(co2.as_str(), 2).unwrap()
