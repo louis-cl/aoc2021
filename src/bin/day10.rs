ca6d34b4e73f5aa0499193576215d949772696bf
Louis Clergue <louisclergue@gmail.com>
2021-12-10 08:29:53 +0100

    Solve day10 p2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use crate::State::{Complete, Corrupted, Incomplete};
 
 #[allow(dead_code)]
@@ -16,7 +17,7 @@ const SAMPLE: &str = r#"
 
 #[derive(Debug)]
 enum State {
-    Incomplete,
+    Incomplete(Vec<char>),
     Corrupted(u32),
     Complete,
 }
@@ -47,40 +48,59 @@ fn main() {
         }
     }
 
-    let p1 = {
-        lines.iter().map(|line| {
-            let mut stack = Vec::with_capacity(line.len());
-            for c in line.chars() {
-                match c {
-                    '('|'['|'{'|'<' => stack.push(c),
-                    ')'|']'|'}'|'>' => {
-                        match stack.last() {
-                            None => return Incomplete,
-                            Some(o) => {
-                                if matching(o, &c) {
-                                    stack.pop();
-                                } else {
-                                    return Corrupted(points(&c));
-                                }
+    let analysis = lines.iter().map(|line| {
+        let mut stack = Vec::with_capacity(line.len());
+        for c in line.chars() {
+            match c {
+                '('|'['|'{'|'<' => stack.push(c),
+                ')'|']'|'}'|'>' => {
+                    match stack.last() {
+                        None => return Incomplete(stack),
+                        Some(o) => {
+                            if matching(o, &c) {
+                                stack.pop();
+                            } else {
+                                return Corrupted(points(&c));
                             }
                         }
-                    },
-                    _ => panic!("unknown char")
-                }
+                    }
+                },
+                _ => panic!("unknown char")
             }
-            if stack.is_empty() {
-                return Complete;
-            }
-            Incomplete
-        }).map(|s| match s {
-            Corrupted(s) => s,
+        }
+        if stack.is_empty() {
+            return Complete;
+        }
+        Incomplete(stack)
+    }).collect_vec();
+
+    let p1 = {
+        analysis.iter().map(|s| match s {
+            Corrupted(s) => *s,
             _ => 0
         }).sum::<u32>()
     };
     println!("p1 = {:?}", p1);
 
     let p2 = {
-
+        let score2 = |c: &char| {
+            match c {
+                '(' => 1,
+                '[' => 2,
+                '{' => 3,
+                '<' => 4,
+                _ => panic!("? {}", c)
+            }
+        };
+        let v = analysis.iter().filter_map(|s| match s {
+            Incomplete(s) => {
+                Some(s.iter().rev().fold(0u64, |acc, x| {
+                    acc * 5 + score2(x)
+                }))
+            }
+            _ => None
+        }).sorted().collect_vec();
+        v[v.len()/2]
     };
     println!("p2 = {:?}", p2);
 }
